00e97b58177c73ff05ea41ba9fe85a6082d09a30
Aashay14 <aashay.korani@sakec.ac.in>
2019-11-28 11:10:32 +0530

    Added firebase

---
+++ app/src/main/java/com/example/speedambulance/SecondPage.java
@@ -0,0 +1,15 @@
+package com.example.speedambulance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+    }
+}
+// LEFT THIS FOR DEVELOPERS PAGE
